d88b45655613280ead97f406526c17bf1cf33db6
TarekSaeed0 <naminode2020@gmail.com>
2025-12-05 19:41:44 +0200

    fix: add attachments storage folder to gitignore, and meant to use path variable for id

--- backend/src/main/java/com/github/oop_assignment_4/controller/AttachmentController.java
+++ backend/src/main/java/com/github/oop_assignment_4/controller/AttachmentController.java
@@ -13,47 +13,45 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/attachments")
 public class AttachmentController {
-  private final AttachmentService attachmentService;
+	private final AttachmentService attachmentService;
 
-  public AttachmentController(AttachmentService attachmentService) {
-    this.attachmentService = attachmentService;
-  }
+	public AttachmentController(AttachmentService attachmentService) {
+		this.attachmentService = attachmentService;
+	}
 
-  @PostMapping("/upload")
-  public ResponseEntity<AttachmentDTO> uploadAttachment(
-      @RequestParam("file") MultipartFile file) {
-    return ResponseEntity.ok(attachmentService.uploadAttachment(file));
-  }
+	@PostMapping("/upload")
+	public ResponseEntity<AttachmentDTO> uploadAttachment(
+			@RequestParam("file") MultipartFile file) {
+		return ResponseEntity.ok(attachmentService.uploadAttachment(file));
+	}
 
-  @GetMapping("/download/{id}")
-  public ResponseEntity<Resource> downloadAttachment(@RequestParam Long id) {
-    Resource resource = attachmentService.downloadAttachment(id);
-    return ResponseEntity.ok()
-        .header(HttpHeaders.CONTENT_DISPOSITION,
-            "inline; filename=\"" + resource.getFilename() + "\"")
-        .body(resource);
-  }
+	@GetMapping("/download/{id}")
+	public ResponseEntity<Resource> downloadAttachment(@PathVariable Long id) {
+		Resource resource = attachmentService.downloadAttachment(id);
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION,
+						"inline; filename=\"" + resource.getFilename() + "\"")
+				.body(resource);
+	}
 
-  @GetMapping
-  public ResponseEntity<List<AttachmentDTO>> getAttachments() {
-    return ResponseEntity.ok(attachmentService.getAttachments());
-  }
+	@GetMapping
+	public ResponseEntity<List<AttachmentDTO>> getAttachments() {
+		return ResponseEntity.ok(attachmentService.getAttachments());
+	}
 
-  @GetMapping("/{id}")
-  public ResponseEntity<AttachmentDTO> getAttachment(@RequestParam Long id) {
-    return ResponseEntity.ok(attachmentService.getAttachment(id));
-  }
+	@GetMapping("/{id}")
+	public ResponseEntity<AttachmentDTO> getAttachment(@PathVariable Long id) {
+		return ResponseEntity.ok(attachmentService.getAttachment(id));
+	}
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<Void> deleteAttachment(@RequestParam Long id) {
-    attachmentService.deleteAttachment(id);
-    return ResponseEntity.noContent().build();
-  }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteAttachment(@PathVariable Long id) {
+		attachmentService.deleteAttachment(id);
+		return ResponseEntity.noContent().build();
+	}
 }
-
